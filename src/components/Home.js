f3dfa2df62ae78c9fb2e67951ecd914e28cc8396
Mark <m.hern1415@gmail.com>
2020-12-09 15:14:45 -0800

    added components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import Plants from './Plants'
+
+const Home = () => {
+  return (
+    <div>
+      <Plants />
+    </div>
+  );
+};
+export default Home;
---
+++ src/components/Plants.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
+class Plants extends Component {
+   
+
+   
+
+render() {
+    return (
+        <div>
+            <h1>Plants bro</h1>
+            
+        </div>
+    )
+}
+
+
+
+
+
+}
+export default Plants;
